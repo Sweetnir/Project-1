8ddf3ca8b13a006efd6d0b668e062a5bda8ebb4e
Damon Herrera <bustinbubs@gmail.com>
2023-03-27 21:29:10 -0700

    nothing is moving yet :(

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ function coloring() {
 
     color.forEach(color => {
 
-        getID = color.id 
-        arr = Array.from(getID)
+        getId = color.id 
+        arr = Array.from(getId)
         arr.shift()
         aside = eval(arr.pop())
         aup = eval(arr.shift())
@@ -44,30 +44,154 @@ function coloring() {
 } 
 coloring()
 
-// function to remove opposing team pieces 
+
+function pinkGreen() {
+    document.querySelectorAll('.box').forEach(i1 => {
+        if (i1.style.backgroundColor === 'pink') {
+
+            document.querySelectorAll('.box').forEach(i2 => {
+
+                if (i2.style.backgroundColor === 'green' && i2.innerText.length !== 0) {
+
+
+                    greenText = i2.innerText
+
+                    pinkText = i1.innerText
+
+                    pinkColor = ((Array.from(pinkText)).shift()).toString()
+                    greenColor = ((Array.from(greenText)).shift()).toString()
+
+                    getId = i2.id
+                    arr = Array.from(getId)
+                    arr.shift()
+                    aside = eval(arr.pop())
+                    aup = eval(arr.shift())
+                    a = aside + aup
+            
+                    if (a % 2 == 0 && pinkColor === greenColor) {
+                        i2.style.backgroundColor = 'rgb(111, 143, 175)'
+                    }
+                    if (a % 2 !== 0 && pinkColor === greenColor) {
+                        i2.style.backgroundColor = 'rgb(137, 207, 240)'
+                    }
+
+                    
+                }
+            })
+        }
+    })
+}
+
+tog = 1
+
+document.querySelectorAll('.box').forEach(item => {
+    
+    item.addEventListener('click', function () {
+// delete opposite element
+if (item.style.backgroundColor === 'green' && item.innerText.length === 0){
+    tog = tog + 1
+}
+else if (item.style.backgroundColor === 'green' && item.innerText.length !== 0) {
+
+    document.querySelectorAll('.box').forEach(i => {
+        if (i.style.backgroundColor === 'pink'){
+            pinkId = i.id
+            pinkText = i.innerText
+
+            document.getElementById(pinkId).innerText = ''
+            item.innerText = pinkText
+            coloring()
+            insertImage()
+            tog = tog + 1
+        }
+
+    })
+}
+    })
+})
+
+getId = item.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        arr.push('0')
+        aup = eval(arr.join(''))
+        a = aside + aup
+
+        //All available paths for every piece
+        function whosTurn(toggle){
+            //Pawn Piece
+        if(item.innerText === `${toggle}pawn`) {
+            item.style.backgroundColor = 'pink'
+
+            if(tog % 2 !== 0 && aup < 800){
+                if(document.getElementById(`b${a + 100}`).innerText.length === 0){
+                document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
+                }
+                if(aside < 8 && document.getElementById(`b${a + 100 + 1}`).innerText.length !== 0 ){
+     document.getElementById(`b${a + 100 + 1}`).style.backgroundColor = 'green'    
+                }
+
+                if(aside > 1 && document.getElementById(`b${a + 100 - 1}`).innerText.length !== 0 ) {
+                    document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'green'   
+                }
+            }
+        }
+        }
+
+        if (tog % 2 === 0 && aup > 100) {
+            if(document.getElementById(`b${a - 100}`).innerText.length === 0){
+                document.getElementById(`b${a - 100}`).style.backgroundColor = 'green'
+            }
+         if(aside < 8 && document.getElementById(`b${a - 100 + 1}`).innerText.length !== 0){
+            document.getElementById(`b${a - 100 + 1}`).style.backgroundColor = 'green'
+         }
+
+         if(aside > 1 && document.getElementById(`b${a - 100 - 1}`).innerText.length !== 0){
+            document.getElementById(`b${a - 100 - 1}`).style.backgroundColor = 'green'
+         }
+
+        }
 
 
 
-// function to see available paths 
 
 
+        //Moving the Element
+        document.querySelectorAll('.box').forEach(moveTest => {
+            moveTest.addEventListener('click', function () {
+                if (moveTest.style.backgroundColor === 'pink'){
 
-// King Piece
+                    pinkId = moveTest.id
+                    pinkText = moveTest.innerText
 
-// Queen Piece
-
-// Rook Piece 
-
-// Knight Piece 
-
-// Bishop Piece 
-
-
-
-
-// Pawn Piece
-
-
+                    document.querySelectorAll('.box').forEach(moveTest2 => {
+                        moveTest2.addEventListener('click', function () {
+                            if(moveTest2.style.backgroundColor === 'green' && 
+                             moveTest2.innerText.length === 0){
+                             document.getElementById(pinkId).innerText = ''
+                             moveTest2.innerText = pinkId
+                             coloring()
+                             insertImage()
+                            }
+                        })
+                    })
+                }
 
 
-// Who's Turn?
+                    
+                })
+            })
+        
+
+        //Whos Turn
+        if (tog % 2 !== 0) {
+            document.getElementById('tog').innerText = "White's Turn"
+            whosTurn('W')
+        }
+         if(tog % 2 === 0) {
+            document.getElementById('tog').innerText = "Black's Turn"
+            whosTurn('B')
+         }
+   
+         pinkGreen()
