c245cdb24c1e6adf7a4219db79abd6aeb1a1ec27
Damon Herrera <bustinbubs@gmail.com>
2023-03-31 20:33:18 -0700

    adding circles and Xs

--- app.js
+++ app.js
@@ -5,11 +5,29 @@ const emptyBoxes = [
 "", "", "", "", "", "", "", "", ""
 ]
 
+let WhosTurn = "circle"
+stuffDisplay.textContent = "circle goes first"
+
 function createBoard(){
     emptyBoxes.forEach((boxes, index) => {
        const boxElement = document.createElement('div')
-       boxElement.classList.add('sqaure')
+       boxElement.classList.add('square')
+       boxElement.id = index
+       boxElement.addEventListener('click', placePiece)
        gameBoard.append(boxElement)
     })
 }
 createBoard()
+
+
+
+function placePiece(e){
+      const goDisplay = document.createElement('div')
+      goDisplay.classList.add(WhosTurn)
+      e.target.append(goDisplay)
+      WhosTurn = WhosTurn === "circle" ? "X" : "circle"
+      stuffDisplay.textContent = " ITS " + WhosTurn + "'s TURN NOW "
+      e.target.removeEventListener('click', placePiece)
+}
+
+
