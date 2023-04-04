ef3709f273aabf4b13d8e966d381911a08936994
Damon Herrera <bustinbubs@gmail.com>
2023-04-03 22:30:56 -0700

    changed code to add reset button

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Project-1
+This is my first Project during my boot camp at UNLV 
+Its the worlds famous Tic-tac-toe!
+It was something simple for my first project
+
+This game consists of HTML, CSS, and JaveScript
+The game doesn't have a score system whenever you reset
+so if you want to add one, you may.
+
+
--- app.js
+++ app.js
@@ -1,46 +1,114 @@
-const gameBoard = document.querySelector('#board')
-const stuffDisplay = document.querySelector('#stuff')
+// grabbing from html 
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.box'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
 
-const emptyBoxes = [
-"", "", "", "", "", "", "", "", ""
-]
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let onGoing = true;
 
-//text at the bottom of board
-let WhosTurn = "circle"
-stuffDisplay.textContent = "circle goes first"
-
-function createBoard(){
-    emptyBoxes.forEach((boxes, index) => {
-       const boxElement = document.createElement('div')
-       boxElement.classList.add('box')
-       boxElement.id = index
-       boxElement.addEventListener('click', placePiece)
-       gameBoard.append(boxElement)
-    })
-}
-createBoard()
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
 
 
-// adding cirlce or X
-function placePiece(e){
-      const goDisplay = document.createElement('div')
-      goDisplay.classList.add(WhosTurn)
-      e.target.append(goDisplay)
-      WhosTurn = WhosTurn === "circle" ? "X" : "circle"
-      stuffDisplay.textContent = " ITS " + WhosTurn + "'s TURN NOW "
-      e.target.removeEventListener('click', placePiece)
-}
+   //winning combos
+    const winningSquares = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ];
 
-//check score
-function score() {
-    const allBoxes = document.querySelectorAll('box')
-    const winningPossibilties = [
-        [0,1,2],[0,3,6],[0,4,8],
-        [1,4,7],[3,4,5],[2,5,8],
-        [8,7,6],[2,4,6]
-    ]
+    function handleResultValidation() {
+        let gameOver = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningSquares[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                gameOver = true;
+                break;
+            }
+        }
 
+    if (gameOver) {
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            onGoing = false;
+            return;
+        }
+
+    if (!board.includes(''))
+        announce(TIE);
     }
 
+    const announce = (type) => {
+        switch(type){
+            case PLAYERO_WON:
+                announcer.innerHTML = ' <span class="playerO">O</span> Won !';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = ' <span class="playerX">X</span> Won !';
+                break;
+            case TIE:
+                announcer.innerText = 'Its a tie';
+        }
+        announcer.classList.remove('hide');
+    };
 
+    const isValidAction = (tile) => {
+        if (tile.innerText === 'X' || tile.innerText === 'O'){
+            return false;
+        }
 
+        return true;
+    };
+
+    const updateBoard =  (index) => {
+        board[index] = currentPlayer;
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && onGoing) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+    // for reset button at the bottom
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        onGoing = true;
+        announcer.classList.add('hide');
+
+        if (currentPlayer === 'O') {
+            changePlayer();
+        }
+
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        });
+    }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+
+    resetButton.addEventListener('click', resetBoard);
+});
